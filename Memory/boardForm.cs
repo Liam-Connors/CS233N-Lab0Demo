9eff3a7d8900ebb215579299260ba58365fc8276
Liam-Connors <connorsl@my.lanecc.edu>
2018-01-17 10:23:27 -0800

    Added all functionality for Memory except ShuffleCards()

--- Memory/boardForm.cs
+++ Memory/boardForm.cs
@@ -65,10 +65,18 @@ namespace Memory
             return GetCardFilename(index).Substring(5, 1);
         }
 
-        // TODO:  students should write this one
+        // This method returns true if the values of the two indexed cards is the same.
+        // Returns false otherwise.
         private bool IsMatch(int index1, int index2)
         {
-            return true;
+            if (GetCardValue(index1) == GetCardValue(index2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         // This method fills each picture box with a filename
@@ -91,6 +99,7 @@ namespace Memory
         // TODO:  students should write this one
         private void ShuffleCards()
         {
+
         }
 
         // This method loads (shows) an image in a picture box.  Assumes that filenames
@@ -112,55 +121,76 @@ namespace Memory
         // shows (loads) the backs of all of the cards
         private void LoadAllCardBacks()
         {
-
+            for (int i = 1; i <= 20; i++)
+            {
+                LoadCardBack(i);
+            }
         }
 
         // Hides a picture box
         private void HideCard(int i)
         {
-
+            GetCard(i).Hide();
         }
 
         private void HideAllCards()
         {
-
+            for(int i = 1; i <= 20; i++)
+            {
+                HideCard(i);
+            }
         }
 
         // shows a picture box
         private void ShowCard(int i)
         {
-
+            GetCard(i).Show();
         }
 
         private void ShowAllCards()
         {
-
+            for (int i = 1; i <= 20; i++)
+            {
+                ShowCard(i);
+            }
         }
 
         // disables a picture box
         private void DisableCard(int i)
         {
-
+            GetCard(i).Enabled = false;
         }
 
         private void DisableAllCards()
         {
-
+            for (int i = 1; i <= 20; i++)
+            {
+                DisableCard(i);
+            }
         }
 
         private void EnableCard(int i)
         {
-
+            GetCard(i).Enabled = true;
         }
 
         private void EnableAllCards()
         {
-
+            for (int i = 1; i <= 20; i++)
+            {
+                EnableCard(i);
+            }
         }
     
         private void EnableAllVisibleCards()
         {
-
+            for (int i = 1; i <= 20; i++)
+            {
+                if(GetCard(i).Visible)
+                {
+                    EnableCard(i);
+                }
+            }
         }
 
         #endregion
@@ -168,14 +198,9 @@ namespace Memory
         #region EventHandlers
         private void boardForm_Load(object sender, EventArgs e)
         {
-            /* 
-             * Fill the picture boxes with filenames
-             * Shuffle the cards
-             * Load all of the card backs - 
-             *      While you're testing you might want to load all of card faces
-             *      to make sure that the cards are loaded successfully and that
-             *      they're shuffled.  If you get all 2s, something is wrong.
-            */
+            FillCardFilenames();
+            //ShuffleCards();
+            LoadAllCardBacks();
         }
 
         private void card_Click(object sender, EventArgs e)
@@ -183,43 +208,48 @@ namespace Memory
             PictureBox card = (PictureBox)sender;
             int cardNumber = int.Parse(card.Name.Substring(4));
 
-            /* 
-             * if the first card isn't picked yet
-             *      save the first card index
-             *      load the card
-             *      disable the card
-             *  else (the user just picked the second card)
-             *      save the second card index
-             *      load the card
-             *      disable all of the cards
-             *      start the flip timer
-             *  end if
-            */
+            if (firstCardNumber == NOT_PICKED_YET)
+            {
+                firstCardNumber = cardNumber;
+                LoadCard(cardNumber);
+                DisableCard(cardNumber);
+            }
+            else
+            {
+                secondCardNumber = cardNumber;
+                LoadCard(cardNumber);
+                DisableAllCards();
+                flipTimer.Start();
+            }
         }
 
         private void flipTimer_Tick(object sender, EventArgs e)
         {
-            /*
-             * stop the flip timer
-             * if the first card and second card are a match
-             *      increment the number of matches
-             *      hide the first card
-             *      hide the second card
-             *      reset the first card number
-             *      reset the second card number
-             *      if the number of matches is 10
-             *          show a message box
-             *      else
-             *          enable all of the cards left on the board
-             *      end if
-             * else
-             *      flip the first card back over
-             *      flip the second card back over
-             *      reset the first card number
-             *      reset the second card number
-             *      enable all of the cards left on the board
-             * end if
-             */
+            flipTimer.Stop();
+            if(IsMatch(firstCardNumber, secondCardNumber))
+            {
+                matches++;
+                HideCard(firstCardNumber);
+                HideCard(secondCardNumber);
+                firstCardNumber = NOT_PICKED_YET;
+                secondCardNumber = NOT_PICKED_YET;
+                if(matches == 10)
+                {
+                    MessageBox.Show("You win!");
+                }
+                else
+                {
+                    EnableAllVisibleCards();
+                }
+            }
+            else
+            {
+                LoadCardBack(firstCardNumber);
+                LoadCardBack(secondCardNumber);
+                firstCardNumber = NOT_PICKED_YET;
+                secondCardNumber = NOT_PICKED_YET;
+                EnableAllVisibleCards();
+            }
         }
         #endregion
     }
